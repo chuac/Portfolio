bda8d84e13477f44cb5d4103f37e7c4cc2214e81
Christopher Chua <chuachristopher@hotmail.com>
2020-08-19 18:36:29 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+require("./assets/main.scss"); // our Sass file, which imports Bulma
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
